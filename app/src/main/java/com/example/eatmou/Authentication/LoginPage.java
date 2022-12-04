010dae899832f5f4c20d7aa71fe7fa17e23eb6c1
TAN ZHI JIAN <jiantanzhi@gmail.com>
2022-12-05 02:09:02 +0800

    Added back home page arrow button for testing

--- app/src/main/java/com/example/eatmou/Authentication/LoginPage.java
+++ app/src/main/java/com/example/eatmou/Authentication/LoginPage.java
@@ -2,15 +2,31 @@ package com.example.eatmou.Authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.example.eatmou.MainActivity;
 import com.example.eatmou.R;
 
 public class LoginPage extends AppCompatActivity {
+
+    ImageView back_home_arrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        back_home_arrow = findViewById(R.id.back_home_arrow);
+
+        back_home_arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/eatmou/Authentication/SignUpPage.java
+++ app/src/main/java/com/example/eatmou/Authentication/SignUpPage.java
@@ -2,13 +2,19 @@ package com.example.eatmou.Authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import com.example.eatmou.MainActivity;
 import com.example.eatmou.R;
 
 public class SignUpPage extends AppCompatActivity {
     Button signUpBtn;
+    ImageView back_home_arrow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +23,14 @@ public class SignUpPage extends AppCompatActivity {
         signUpBtn = findViewById(R.id.signUpBtn);
 
         signUpBtn.setBackground(getResources().getDrawable(R.drawable.submitbtnbg));
+
+        back_home_arrow = findViewById(R.id.back_home_arrow);
+        back_home_arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
     }
 }
