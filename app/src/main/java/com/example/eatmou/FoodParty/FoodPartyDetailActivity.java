ef6e7fb9129a6fdd244aaf70d82433092a76e938
eren <erenkuek1106@gmail.com>
2022-12-06 01:08:43 +0800

    Food party detail top part ui

---
+++ app/src/main/java/com/example/eatmou/FoodParty/FoodPartyDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.eatmou.FoodParty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.eatmou.R;
+
+public class FoodPartyDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_party_detail);
+    }
+}
