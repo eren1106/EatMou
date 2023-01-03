11a9a224ac9d8311b60bad2fc45b7734fb081ec3
TAN ZHI JIAN <jiantanzhi@gmail.com>
2023-01-03 14:25:09 +0800

    Testing

--- app/src/main/java/com/example/eatmou/ui/homePage/HomeFragment.java
+++ app/src/main/java/com/example/eatmou/ui/homePage/HomeFragment.java
@@ -34,6 +34,8 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d("Testing","Testing");
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         user_matching_list = view.findViewById(R.id.user_matching_list);
