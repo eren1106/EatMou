043777b1e2143c11d7d26f2289e492965cee1ebc
TAN ZHI JIAN <jiantanzhi@gmail.com>
2023-01-04 02:10:07 +0800

    Done the UI of manage password page

---
+++ app/src/main/java/com/example/eatmou/ui/Authentication/appLock/AppLock.java
@@ -0,0 +1,16 @@
+package com.example.eatmou.ui.Authentication.appLock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.eatmou.R;
+
+public class AppLock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_lock);
+    }
+}
